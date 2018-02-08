db8e4813c0d695a6c94cf8a8968e600f6466d81d
Olivier Boily <35646923+oboily@users.noreply.github.com>
2018-02-07 22:36:54 -0500

    Add files via upload

---
+++ isdmxinjail.ps1
@@ -0,0 +1,20 @@
+﻿function IsDMXInJail {
+      <#
+      .SYNOPSIS
+      Is DMX in jail ? Good question!
+      .DESCRIPTION
+      Return True if DMX is in Jail
+      .EXAMPLE
+      if (Get-DMXPrisonStatut -eq $True){Write-Host("DMX est en prison!")}
+      else{Write-Host("DMX est libre!")}
+      #>
+    $IsDmxInJail = Invoke-WebRequest -Uri 'http://isdmxinjail.com/'
+    foreach($ligne in $IsDmxInJail.tostring().split("`n"))
+    {
+        if($ligne -like "*<h1>DMX*")
+        {
+            if($ligne -like "*definitely*"){return $True} # If DMX is in jail
+            if($ligne -like "*not*"){return $False} # If DMX is not in jail
+        }
+    }
+}
